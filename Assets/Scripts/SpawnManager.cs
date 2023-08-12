a3a59837a04a4c0984556226a7c567ff5e82dc21
José Pedretti Neto <jpedretti@outlook.com>
2023-08-12 16:43:46 -0400

    person, hunter and weapons spawn configured

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject hunterGround;
+    public GameObject hunterSky;
+    public GameObject personGround;
+    public GameObject personSky;
+
+    private const float hunterSpawnInterval = 3;
+    private const float personSpawnInterval = 2;
+    private const float spawnPositionX = 26;
+    private readonly Vector3 groundSpawnPosition = new (spawnPositionX, -13.5f, 0);
+    private const float skySpawnPositionMaxY = 14.5f;
+    private const float skySpawnPositionMinY = -11f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnHunter), 2, hunterSpawnInterval);
+        InvokeRepeating(nameof(SpawnPerson), 2, personSpawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnHunter()
+    {
+        SpawnPrefab(hunterGround, hunterSky);
+    }
+
+    private void SpawnPerson()
+    {
+        SpawnPrefab(personGround, personSky);
+    }
+
+    private void SpawnPrefab(GameObject prefabGround, GameObject prefabSky)
+    {
+        if (Random.Range(0, 2) == 0)
+        {
+            Instantiate(prefabGround, groundSpawnPosition, prefabGround.transform.rotation);
+        }
+        else
+        {
+            Instantiate(prefabSky, GetRandomSkySpawnPosition(), prefabSky.transform.rotation);
+        }
+    }
+
+    private Vector3 GetRandomSkySpawnPosition()
+    {
+        return new Vector3(spawnPositionX, Random.Range(skySpawnPositionMaxY, skySpawnPositionMinY), 0);
+    }
+}

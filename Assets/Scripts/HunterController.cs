3185caf6b699909bec9bfd1bb9016b539fe7acdc
José Pedretti Neto <jpedretti@outlook.com>
2023-08-12 15:37:55 -0400

    fix missing prefab

--- Assets/Scripts/HunterController.cs
+++ Assets/Scripts/HunterController.cs
@@ -8,14 +8,14 @@ public class HunterController : MonoBehaviour
 {
     private GameObject player;
 
-    public GameObject weaponPreFab;
+    //public GameObject weaponPreFab;
     public float speed = 20;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag(Tags.Player);
-        InvokeRepeating(nameof(SpawnWeapon), 1, 1);
+        //InvokeRepeating(nameof(SpawnWeapon), 1, 1);
 
     }
 
@@ -25,15 +25,15 @@ public class HunterController : MonoBehaviour
         
     }
 
-    private void SpawnWeapon()
-    {
-        var weaponInstance = Instantiate(weaponPreFab, transform.position, weaponPreFab.transform.rotation);
+    //private void SpawnWeapon()
+    //{
+    //    var weaponInstance = Instantiate(weaponPreFab, transform.position, weaponPreFab.transform.rotation);
 
 
-        float AngleRad = Mathf.Atan2(player.transform.position.y - transform.position.y, player.transform.position.x - transform.position.x);
-        // Get Angle in Degrees
-        float AngleDeg = (180 / Mathf.PI) * AngleRad;
-        // Rotate Object
-        weaponInstance.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
-    }
+    //    float AngleRad = Mathf.Atan2(player.transform.position.y - transform.position.y, player.transform.position.x - transform.position.x);
+    //    // Get Angle in Degrees
+    //    float AngleDeg = (180 / Mathf.PI) * AngleRad;
+    //    // Rotate Object
+    //    weaponInstance.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+    //}
 }

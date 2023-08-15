72d152fae6c9e8d28030731def7cfe96eb628c1c
José Pedretti Neto <jpedretti@outlook.com>
2023-08-15 19:18:31 -0400

    add assets to persons

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -11,9 +11,10 @@ public class SpawnManager : MonoBehaviour
     private const float hunterSpawnInterval = 3;
     private const float personSpawnInterval = 2;
     private const float spawnPositionX = 26;
-    private readonly Vector3 groundSpawnPosition = new(spawnPositionX, -12.1f, 0);
-    private const float skySpawnPositionMaxY = 14.5f;
-    private const float skySpawnPositionMinY = -11f;
+    private readonly Vector3 hunterGroundSpawnPosition = new(spawnPositionX, -12.1f, 0);
+    private readonly Vector3 personGroundSpawnPosition = new(spawnPositionX, -13.0f, 0);
+    private const float skySpawnPositionMaxY = 10.5f;
+    private const float skySpawnPositionMinY = -8f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,15 +25,15 @@ public class SpawnManager : MonoBehaviour
 
     private void SpawnHunter()
     {
-        SpawnPrefab(hunterGround, hunterSky);
+        SpawnPrefab(hunterGround, hunterSky, hunterGroundSpawnPosition);
     }
 
     private void SpawnPerson()
     {
-        SpawnPrefab(personGround, personSky);
+        SpawnPrefab(personGround, personSky, personGroundSpawnPosition);
     }
 
-    private void SpawnPrefab(GameObject prefabGround, GameObject prefabSky)
+    private void SpawnPrefab(GameObject prefabGround, GameObject prefabSky, Vector3 groundSpawnPosition)
     {
         if (Random.Range(0, 2) == 0)
         {
